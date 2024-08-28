31fbc56a3903f4913c0378dfe00c6fee8d0220f8
Thura Myint <thuramyint@192.168.123.4>
2024-08-28 16:38:17 +0630

    change private Key

--- configs/firebase.js
+++ configs/firebase.js
@@ -1,6 +1,6 @@
 const admin = require('firebase-admin');
 
-const serviceAccount = require('../../../private key/to-do-list-afa5e-firebase-adminsdk-e2z5d-28069301cd.json');
+const serviceAccount = require('../privateKey');
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
